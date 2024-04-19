7fab9e6f51e8f15332038ac4bdb2dc1138f4aac8
Edward <60502285+ethndo@users.noreply.github.com>
2024-04-19 15:19:18 +1000

    Refactored games.jsx to use 2 spacing

--- client/pages/games.jsx
+++ client/pages/games.jsx
@@ -13,135 +13,142 @@ const games = [
     { name: 'Line Four', description: 'Connect Four!', status: false, imageName: '/line_four.svg', colour: '#33D9B2', detailedInfo: 'Two rival players go head to head dropping tokens in a grid, fighting to be the first to form a horizontal, vertical or diagonal line of four.', numPlayers: '2'},
 ];
 
+/**
+ * Games: main landing page for all games available to be played.
+ * @returns 
+ */
 const Games = () => {
-    const router = useRouter();
-    const nickname = router.query;
-    const [openDialog, setOpenDialog] = useState(null);
-    const [alertOpen, setAlertOpen] = useState(false);
-    const [alertMessage, setAlertMessage] = useState('');
+  const router = useRouter();
+  const nickname = router.query;
+  const [openDialog, setOpenDialog] = useState(null);
+  const [alertOpen, setAlertOpen] = useState(false);
+  const [alertMessage, setAlertMessage] = useState('');
 
-    // Clicking home button
-    const handleClick = () => {
-        router.push({
-          pathname: '/',
-        });
-    };
+  // Clicking home button
+  const handleClick = () => {
+    router.push({
+      pathname: '/',
+    });
+  };
 
-    // Clicking on a game
-    const handleClickOpenDialog = (gameName) => {
-        setOpenDialog(gameName);
-    }
-    const handleCloseDialog = () => {
-        setOpenDialog(null);
-        setAlertOpen(false);
-    }
+  // Clicking on a game
+  const handleClickOpenDialog = (gameName) => {
+    setOpenDialog(gameName);
+  }
 
-    // Clicking play
-    const handleClickPlay = (game) => {
-        if (game.status) {
-            const gamePath = `/game/${game.name.replace(/\s+/g, '-').toLowerCase()}?nickname=${encodeURIComponent(nickname.nickname || '')}`;
-            router.push(gamePath);
-        } else {
-            setAlertMessage('Sorry this game is currently unavailable.');
-            setAlertOpen(true);
-        }
+  // Handles closing the game information dialog
+  const handleCloseDialog = () => {
+    setOpenDialog(null);
+    setAlertOpen(false);
+  }
+
+  // Clicking play
+  const handleClickPlay = (game) => {
+    if (game.status) {
+      const gamePath = `/game/${game.name.replace(/\s+/g, '-').toLowerCase()}?nickname=${encodeURIComponent(nickname.nickname || '')}`;
+      router.push(gamePath);
+    } else {
+      setAlertMessage('Sorry this game is currently unavailable.');
+      setAlertOpen(true);
     }
-    // Retrieve games
+  }
+  
+  // TODO: Retrieve the available games from the backend as opposed to the hardcoded object at the top of the file.
 
     return (
-        <Box sx={{ flexGrow: 1, p: 3 }}>
-            <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: 2 }}>
-                <IconButton aria-label="home" size="large" onClick={handleClick}>
-                    <HomeIcon fontSize="inherit"/>
-                </IconButton>
+      <Box sx={{ flexGrow: 1, p: 3 }}>
+        <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: 2 }}>
+          <IconButton aria-label="home" size="large" onClick={handleClick}>
+            <HomeIcon fontSize="inherit"/>
+          </IconButton>
                 
-                <Typography
-                    variant="h4"
-                    component="h1"
-                    gutterBottom
-                    sx={{
-                        fontFamily: 'JetBrains Mono, monospace',
-                        color: '#66509F',
-                    }}
-                    >
-                    Available Games
-                </Typography>
-                <Box sx={{ width: 48, height: 48 }}/>
-            </Box>
-            
-            <Grid container spacing={4}>
-                {games.map((game, index) => (
-                <Grid item xs={12} sm={6} md={4} key={index}>
-                    <Card sx={{ "&:hover": { boxShadow: 8 } }}>
-                        <CardActionArea onClick={() => handleClickOpenDialog(game.name)}>
-                            <CardMedia
-                            component="img"
-                            height="140"
-                            image={`/logos${game.imageName}`}
-                            alt={game.name}
-                            sx={{
-                                objectFit: 'contain',
-                            }}
-                            />
-                            <CardContent>
-                            <Typography gutterBottom variant="h5" component="div" style={{ fontFamily: 'JetBrains Mono, monospace' }}>
-                                {game.name}
-                            </Typography>
-                            <Typography variant="body2" color="text.secondary" style={{ fontFamily: 'JetBrains Mono, monospace' }}>
-                                {game.description}
-                            </Typography>
-                            </CardContent>
-                        </CardActionArea>
-                    </Card>
-                    <Dialog open={openDialog === game.name} onClose={handleCloseDialog} maxWidth="sm" fullWidth>
-                        {alertOpen && (
-                            <Alert 
-                                severity="error" 
-                                onClose={() => setAlertOpen(false)}
-                                sx={{ marginBottom: 2 }}
-                            >
-                                {alertMessage}
-                            </Alert>
-                        )}
-                        <Box sx={{ padding: 2 }}>
-                            {
-                                !game.status && (
-                                <Chip label="Coming Soon!" color="secondary"/>
-                            )}
-                            <Box sx={{ display: 'flex', justifyContent: 'left', alignItems: 'center', gap: 1 }}>
-                                <DialogTitle sx={{ flex: 1 }}>{game.name}</DialogTitle>
-                                <PeopleIcon />
-                                <DialogContentText>
-                                    {game.numPlayers} player(s)
-                                </DialogContentText>
-                            </Box>
-                            <DialogContent>
-                                <DialogContentText>
-                                    {game.detailedInfo}
-                                </DialogContentText>
-                            </DialogContent>
-                            <Button
-                                fullWidth
-                                variant="contained"
-                                endIcon={<PlayCircleOutlineIcon />}
-                                sx={{
-                                    mt: 2,
-                                    backgroundColor: '#66509F',
-                                    '&:hover': {
-                                        backgroundColor: '#56427C',
-                                    },
-                                }}
-                                onClick={() => handleClickPlay(game)}
-                            >
-                                Play
-                            </Button>
-                        </Box>
-                    </Dialog>
-                </Grid>
-                ))}
-            </Grid>
+          <Typography
+            variant="h4"
+            component="h1"
+            gutterBottom
+            sx={{
+              fontFamily: 'JetBrains Mono, monospace',
+              color: '#66509F',
+            }}
+          >
+            Available Games
+          </Typography>
+          <Box sx={{ width: 48, height: 48 }}/>
         </Box>
-      );
+            
+        <Grid container spacing={4}>
+          {games.map((game, index) => (
+          <Grid item xs={12} sm={6} md={4} key={index}>
+            <Card sx={{ "&:hover": { boxShadow: 8 } }}>
+              <CardActionArea onClick={() => handleClickOpenDialog(game.name)}>
+                <CardMedia
+                  component="img"
+                  height="140"
+                  image={`/logos${game.imageName}`}
+                  alt={game.name}
+                  sx={{
+                      objectFit: 'contain',
+                  }}
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div" style={{ fontFamily: 'JetBrains Mono, monospace' }}>
+                    {game.name}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary" style={{ fontFamily: 'JetBrains Mono, monospace' }}>
+                      {game.description}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+            <Dialog open={openDialog === game.name} onClose={handleCloseDialog} maxWidth="sm" fullWidth>
+              {alertOpen && (
+                  <Alert 
+                      severity="error" 
+                      onClose={() => setAlertOpen(false)}
+                      sx={{ marginBottom: 2 }}
+                  >
+                      {alertMessage}
+                  </Alert>
+              )}
+              <Box sx={{ padding: 2 }}>
+                {
+                  !game.status && (
+                  <Chip label="Coming Soon!" color="secondary"/>
+                )}
+                <Box sx={{ display: 'flex', justifyContent: 'left', alignItems: 'center', gap: 1 }}>
+                  <DialogTitle sx={{ flex: 1 }}>{game.name}</DialogTitle>
+                  <PeopleIcon />
+                  <DialogContentText>
+                      {game.numPlayers} player(s)
+                  </DialogContentText>
+                </Box>
+                <DialogContent>
+                  <DialogContentText>
+                      {game.detailedInfo}
+                  </DialogContentText>
+                </DialogContent>
+                <Button
+                  fullWidth
+                  variant="contained"
+                  endIcon={<PlayCircleOutlineIcon />}
+                  sx={{
+                    mt: 2,
+                    backgroundColor: '#66509F',
+                    '&:hover': {
+                        backgroundColor: '#56427C',
+                    },
+                  }}
+                  onClick={() => handleClickPlay(game)}
+                >
+                  Play
+                </Button>
+              </Box>
+            </Dialog>
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
+  );
 };
 
 export default Games;
