2e0e9ee9e43c5e63ca9520e0750557d4cb86ecb4
Edward <60502285+ethndo@users.noreply.github.com>
2024-04-27 15:28:57 +1000

    Update apiProvider and fetch for correct API

--- client/contexts/apiProvider.jsx
+++ client/contexts/apiProvider.jsx
@@ -5,13 +5,13 @@ const ApiContext = createContext(null);
 export const useApi = () => useContext(ApiContext);
 
 export const ApiProvider = ({ children }) => {
-  const defaultApiUrl = 'http://localhost:8000/api';
+  const defaultApiUrl = 'http://localhost:8000';
   const apiUrl = process.env.NEXT_PUBLIC_API_URL || defaultApiUrl;
 
   // Function to make GET requests
   const get = async (endpoint) => {
     try {
-      const response = await fetch(`${apiUrl}/${endpoint}`);
+      const response = await fetch(`${apiUrl}${endpoint}`);
       if (!response.ok) throw new Error('Network response was not ok');
       return await response.json();
     } catch (error) {
